bc3cac1e7328f0c56bc700c228437fbc475e7f5d
Alexander Baranov <albar@joybits.org>
2017-04-14 22:58:32 +0300

    MainMenu, mascot

---
+++ webinar2/MainMenu.swift
@@ -0,0 +1,16 @@
+//
+//  MainMenu.swift
+//  webinar2
+//
+//  Created by trbrmrdr on 14/04/2017.
+//  Copyright © 2017 Poloz. All rights reserved.
+//
+
+import SpriteKit
+
+class MainMenu: SKScene {
+    override func didMove(to view: SKView) {
+        print("MainMenu running!")
+    }
+
+}
