ea1b9072d11d9bda56006308ec36041eb6ee1cb2
Alexander Baranov <albar@joybits.org>
2017-04-20 23:35:10 +0300

    Parents

--- webinar2/GameViewController.swift
+++ webinar2/GameViewController.swift
@@ -36,18 +36,7 @@ class GameViewController: UIViewController {
         return true
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = MainMenu(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene, transition: SKTransition.crossFade(withDuration: 1.0))
-            }
-        }
-    }
+
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
--- webinar2/MainMenu.swift
+++ webinar2/MainMenu.swift
@@ -10,7 +10,27 @@ import SpriteKit
 
 class MainMenu: SKScene {
     override func didMove(to view: SKView) {
-        print("MainMenu running!")
+        //print("MainMenu running!")
+        let title:SKLabelNode = self.childNode(withName: "title") as! SKLabelNode
+        let mascot:SKSpriteNode = title.childNode(withName: "mascot") as! SKSpriteNode
+        //mascot.removeFromParent()
+        //self.removeAllChildren()
+        //title.addChild(mascot)
+        //mascot.position = CGPoint(x: 0, y: -120)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = MainMenu(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene, transition: SKTransition.crossFade(withDuration: 1.0))
+            }
+        }
     }
 
 }
